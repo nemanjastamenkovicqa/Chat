75b9b1a8cdc8ad9b2e56704e780ca632d3974299
nemanjastamenkovicqa <nemanjastamenkovicqa@gmail.com>
2024-02-12 21:38:30 +0100

    Added PrivateMessageService for user-to-user messaging. Users can now send private messages. Enhances communication options in Simple Chat Application.

---
+++ src/main/java/PermissionManager.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class PermissionManager {
+    private Map<User, String> userPermissions;
+
+    public PermissionManager() {
+        this.userPermissions = new HashMap<>();
+    }
+
+    public void setUserPermission(User user, String permission) {
+        userPermissions.put(user, permission);
+    }
+
+    public String getUserPermission(User user) {
+        return userPermissions.getOrDefault(user, "DEFAULT");
+    }
+}
